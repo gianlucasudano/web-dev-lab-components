2d268adb336d8a92f6044227604aff1f09a23e51
Gianluca Sudano <sudanogianluca@gmail.com>
2024-02-15 19:31:47 +0100

    exported tokens, imported inline into Stacked card styles

--- lib/main.ts
+++ lib/main.ts
@@ -1,7 +1,5 @@
 export { StackedCard } from "./components/Cards/StackedCard";
 
 import * as tokens from "./theme/tokens.css";
-import * as themeLight from "./theme/theme.light.css";
-// import * as typography from "./theme/typography.module.css";
-// import * as colors from "./theme/colors.module.css";
-export { tokens, themeLight };
+
+export { tokens };
